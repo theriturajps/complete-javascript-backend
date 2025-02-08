b0aa679b1bef20497a0d83d4f6097396038c8c32
Ritu Raj Pratap Singh <theriturajps@gmail.com>
2025-02-08 19:34:59 +0530

    fix: improve error handling and logging in MongoDB connection

--- complete-backend/src/db/connect.js
+++ complete-backend/src/db/connect.js
@@ -4,10 +4,9 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async (URI) => {
 	try {
 		await mongoose.connect(URI + '/' + DB_NAME)
-		console.log(`mongodb connected!! DB NAME: ${mongoose.connection.readyState}`);
+		console.log(`mongodb connected!! DB NAME: ${mongoose.connection.readyState}`)
 	} catch (error) {
-		console.log("mongodb connection failed :- ", error);
-		throw new Error(error);
+		console.log("mongodb connection failed :- ", error)
 		process.exit(1)
 	}
 }
